60524d9a715a9d6462199ea4f3bcab45c4aa325f
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-10 11:05:21 -0800

    Try adding in a multiplication formula in event.prevenDefault.

--- src/index.js
+++ src/index.js
@@ -8,29 +8,38 @@ import ExchangeService from './js/ExchangeService';
 function getExchange(country) {
   ExchangeService.getExchange(country)
     .then(function(response) {
-      if(response.main) {
-        printElements(response, country);
+      if(response.conversion_rate) {
+        printElements(response);
       } else {
-        printError(response);
+        printError();
       }
     });
 }
 
 // UI Logic
 
-function printElements(response) {
-  document.querySelector('#showExchangeRates').innerText = `The current exchange rate is ${response.main}.`;
+function printElements(actualAmount) {
+  document.querySelector('#showExchangeRates').innerText = `The converted amount is ${actualAmount}.`;
 }
 
+// function printElements(response) {
+//   // document.querySelector('#showExchangeRates').innerText = `The current exchange rate is ${response.conversion_rate}.`;
+// }
+
 function printError() {
-  document.querySelector('#showExchangeRates').innerText = `There was an error ${error}.`;
+  document.querySelector('#showExchangeRates').innerText = `There was an error.`; // update this
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
   const amount = document.querySelector('#number-input').value;
+  const country = document.querySelector('#currency-type').value;
   document.querySelector('#number-input').value = null;
-  getExchange(amount);
+  getExchange(country);
+
+  let actualAmount = (amount * getExchange(country));
+  return actualAmount;
+
 }
 
 window.addEventListener("load", function() {

e1f0c5e041636bb9decd77e36a5692ef4e715722
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-02-10 13:59:24 -0800

    Try updating error messages for custom error message, won't work because still can't access API -- still receiving 404 error. Deleted test files.

--- src/js/ExchangeService.js
+++ src/js/ExchangeService.js
@@ -3,8 +3,9 @@ export default class ExchangeService {
   static getExchange(country, amount) {
     return fetch(`https://v6.exchangerate-api.com/v6/{process.env.API_KEY}/pair/USD/{country}/{amount}`)
       .then(function(response) {
+        console.log(response);
         if (!response.ok) {
-          const errorMessage = `There was an error`; //update this to be specific
+          const errorMessage = `${response.status} ${response.statusText}`;
           throw new Error(errorMessage);
         } else {
           return response.json();
